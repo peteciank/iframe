c9bbfc67955a1e7ca8cc24599cf3a8c12407570c
peteciank <106826936+peteciank@users.noreply.github.com>
2022-08-10 12:58:21 -0300

    Update iframe.py

--- iframe.py
+++ iframe.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 p = open("map.html")
-components.html(p.read(), height="850", width="1280")
+components.html(p.read(), height=850, width=1280, scrolling=True)
